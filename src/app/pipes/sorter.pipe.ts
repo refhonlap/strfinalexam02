93524e4fbea6aebf3e3a6b51be951c7459fe53b5
Davdi <n.davdi@gmail.com>
2021-03-25 18:58:29 +0100

    Final :)

--- src/app/pipes/sorter.pipe.ts
+++ src/app/pipes/sorter.pipe.ts
@@ -5,8 +5,25 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SorterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: null | any[], sortby: string): any[] | null {
+
+    if (!Array.isArray(value) || !sortby) {
+      return value;
+    }
+
+    if (sortby==='') return value;
+    else {
+      return value.sort( (a, b) => {
+        if ( a[sortby] < b[sortby] ){
+          return -1;
+        }
+        if ( a[sortby] > b[sortby] ){
+          return 1;
+        }
+        return 0;
+      });
+    }
+
   }
 
 }
